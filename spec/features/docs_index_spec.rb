cacb267d69951d6c7bb64abb4227270a459e36d0
Ben <ben.challenor@btopenworld.com>
2018-05-25 16:44:17 +0100

    Updated document index test

--- spec/features/docs_index_spec.rb
+++ spec/features/docs_index_spec.rb
@@ -4,10 +4,10 @@ feature 'Docs index' do
   scenario 'Page has list of docs' do
     visit('/docs/new')
     fill_in 'Title', with: 'test doc'
-    fill_in 'Content', with: 'test doc'
+    fill_in 'Content', with: 'test document'
     click_button 'Create Doc'
     visit('/docs')
-    expect(page).to have_content('test doc')
-    expect(page).to have_content('test doc')
+    expect(page).to have_link('test doc')
+    expect(page).to have_content('test document')
   end
 end
