6b4ab2063c3c299578a2bf527b53f6482fe4ca94
Ben <ben.challenor@btopenworld.com>
2018-05-26 17:12:27 +0100

    Failing test for deleteing a doc

---
+++ spec/features/delete_spec.rb
@@ -0,0 +1,16 @@
+require 'rails_helper.rb'
+
+feature 'Delete' do
+  scenario 'Doc can be deleted' do
+    visit('/docs/new')
+    fill_in 'Title', with: 'Delete test doc'
+    fill_in 'Content', with: 'Delete test document'
+    click_button 'Create Doc'
+    click_link 'Delete Doc'
+    page.accept_alert 'Are you sure?' do
+      click_button('OK')
+    end
+    expect(page).to have_no_content('Delete test doc')
+    expect(page).to have_no_content('Delete test document')
+  end
+end
